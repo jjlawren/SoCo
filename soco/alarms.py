693016e398a3ea9653070dfbec14dd540483f8a7
Jason Lawrence <jjlawren@users.noreply.github.com>
2021-08-02 12:26:56 -0500

    Shortcut to calculate and store next alarm list ID

--- soco/alarms.py
+++ soco/alarms.py
@@ -355,6 +355,8 @@ class Alarm:
             response = self.zone.alarmClock.CreateAlarm(args)
             self.alarm_id = int(response["AssignedID"])
             alarms = Alarms()
+            if alarms.last_id == self.alarm_id - 1:
+                alarms.last_alarm_list_version = f"{alarms.last_uid}:{self.alarm_id}"
             alarms.alarms[self.alarm_id] = self
         else:
             # The alarm has been saved before. Update it instead.
